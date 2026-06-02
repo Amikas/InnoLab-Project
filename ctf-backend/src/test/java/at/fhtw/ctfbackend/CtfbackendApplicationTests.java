d677c29dcbaab8b9247ed7ee7f2b38b4863e7967
Amikas <aminkasmi007@gmail.com>
2026-06-02 23:20:56 +0200

    fix: improve pipeline backend tests

--- ctf-backend/src/test/java/at/fhtw/ctfbackend/CtfbackendApplicationTests.java
+++ ctf-backend/src/test/java/at/fhtw/ctfbackend/CtfbackendApplicationTests.java
@@ -2,8 +2,10 @@ package at.fhtw.ctfbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "JWT_SECRET=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+@ActiveProfiles("test")
 class CtfbackendApplicationTests {
 
 	@Test
